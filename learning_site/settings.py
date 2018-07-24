63a7a217cafa8198bab1e5bda0ac74b98f9ce3ca
Rafael Risco <rafaelrisconardiz@gmail.com>
2018-07-23 20:45:39 -0500

    add deploy settings

--- learning_site/settings.py
+++ learning_site/settings.py
@@ -12,6 +12,17 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+def get_env_variable(var_name):
+    """Get the environment variable or return exception"""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the {} env variable".format(var_name)
+        if DEBUG:
+            warning.warn(error_msg)
+        else:
+            raise ImproperlyConfigured(error_msg)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,6 +144,8 @@ REST_FRAMEWORK = {
     )
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
