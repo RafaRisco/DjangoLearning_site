7789543a43c244cd1f0b6b89e7045c2748dea6db
Rafael Risco <rafaelrisconardiz@gmail.com>
2018-07-25 19:11:15 -0500

    adjust at settings

--- learning_site/settings.py
+++ learning_site/settings.py
@@ -36,7 +36,9 @@ SECRET_KEY = 'vg3=@d+je_)%g)^6rsld&%(0)t=+c13!$q5*6!p38%)d7-u7j!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.pythonanywhere.com',
+    ]
 
 
 # Application definition
